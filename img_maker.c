bfc3b00a26aba7c24a7f759e66c7b933ece87bdc
Jozsef Kiraly <fonix232@gmail.com>
2013-06-23 18:01:45 +0200

    Fixed a small typo in img_maker

--- img_maker.c
+++ img_maker.c
@@ -183,9 +183,10 @@ void usage(const char *appname) {
 			"%s [chiptype] [loader] [major ver] [minor ver] [subver] [old image] [out image]\n\n"
 			"Example:\n"
 			"%s -rk30 Loader.bin 1 0 23 rawimage.img rkimage.img \tRK30 board\n"
+			"%s -rk31 Loader.bin 4 0 4 rawimage.img rkimage.img \tRK31 board\n"
 			"\n\n"
 			"Options:\n"
-			"[chiptype]:\n\trk29\n\trk30\n\trk31",p,p);
+			"[chiptype]:\n\t-rk29\n\t-rk30\n\t-rk31",p,p);
 }
 
 int main(int argc, char **argv)
@@ -193,15 +194,15 @@ int main(int argc, char **argv)
 	// loader, majorver, minorver, subver, oldimage, newimage
 	if (argc == 8)
 	{
-		if (strcmp(argv[1], "rk29") == 0)
+		if (strcmp(argv[1], "-rk29") == 0)
 		{
 			pack_rom(0x50, argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), argv[6], argv[7]);
 		}
-		else if (strcmp(argv[1], "rk30") == 0)
+		else if (strcmp(argv[1], "-rk30") == 0)
 		{
 			pack_rom(0x60, argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), argv[6], argv[7]);
 		}
-		else if (strcmp(argv[1], "rk31") == 0)
+		else if (strcmp(argv[1], "-rk31") == 0)
 		{
 			pack_rom(0x70, argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), argv[6], argv[7]);
 		}
